b63f36bf1f44f6da0df12170fdaa4c83c5e9471e
xiaozhuweixz <cxw@boozinfo.com>
2018-04-07 23:02:50 +0800

    json\Xml 学习文件

---
+++ pythonDay2/pythonLianxi.py
@@ -0,0 +1,27 @@
+import getpass
+# name = input('请输入用户名：');
+# pwd = input('请输入密码：');
+# if name == "alex" and pwd == "cmd":
+#     print("欢迎，alex！")
+# else:
+#     print("用户名和密码错误")
+# i = 0;
+# while 10 > i :
+#     i = i+1;
+#     if i == 7:
+#         continue;
+#     else:
+#         print(i)
+x = 1
+y = 0
+while True:
+     y = y + x
+     #print(x)
+     if x == 100:
+        break
+     x += 1
+     print(y)
+
+
+
+
